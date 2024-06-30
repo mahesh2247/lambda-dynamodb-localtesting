6a74a0470f4318eaecdc0fdb5d7f6b89c32ddb5b
Mahesh M <maheshm@Maheshs-Laptop.local>
2024-06-30 14:24:32 +0530

    added routes for GET and POST

--- src/app.py
+++ src/app.py
@@ -2,28 +2,47 @@ import json
 import boto3
 import os
 import traceback
+import uuid
 from botocore.exceptions import ClientError
 
 # import requests
 
 
+def scanRescursive(ddb, **kwargs):
+    response = ddb.scan(**kwargs)
+    data = response.get('Items')
+    while 'LastEValuatedKey' in response:
+        response = kwargs.get('table').scan(ExclusiveStartKey=response['LastEvaluatedKey'], **kwargs)
+        data.extend(response['Items'])
+    return data
+    
 
 def lambda_handler(event, context):
     
     table_name = os.environ['TABLE']
-    region = 'us-east-1'
     person_tbl = boto3.resource('dynamodb', endpoint_url="http://docker.for.mac.localhost:8000/").Table(table_name)
     try:
         if event['httpMethod'] == 'GET':
+            data = scanRescursive(ddb=person_tbl)
             return {
                 'statusCode': 200,
-                'body': 'Hello from Lambda!'
+                'body': data
             }
-        else:
+        elif event['httpMethod'] == 'POST':
             body_dict = json.loads(event['body'])
+            print(body_dict)
+            p_id = str(uuid.uuid4())
+            person_tbl.put_item(
+                Item={
+                    'Id': p_id,
+                    'FirstName': body_dict['FirstName'],
+                    'LastName': body_dict['LastName']
+                }
+            )
+            print("Item inserted!")
             return {
-                'statusCode': 200,
-                'body': body_dict
+                'statusCode': 201,
+                'body': p_id
             }
     except Exception as e:
         return {'statusCode': 400, 'body': {e}}
